1a1bf76072ca65872ac0961a4cdb6d4dd5de3f13
LeGion <mehrdad.dadashi@gmail.com>
2024-04-30 19:22:19 +0300

    relationships done.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,29 @@ class Post extends Model
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+
+    }
+
+    public function setTitleAttribute($value)
+    {
+        return $this->attributes['title'] = strtolower($value);
+    }
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+
+    }
+
 }
